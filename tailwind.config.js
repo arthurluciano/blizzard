3b0e984dc7a90a1d9cff588d7a06a431b25dd617
arthurluciano <arthurluciano7@gmail.com>
2023-01-04 19:13:42 -0300

    feat: init base config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
